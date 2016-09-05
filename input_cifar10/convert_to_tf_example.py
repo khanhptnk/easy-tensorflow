9b5b9ecb817de1f4c05540edd28528c3812cf06c
Khanh Nguyen <nguyenxuankhanhm@gmail.com>
2016-09-05 19:27:05 +0700

    Add comments to convert to tf example

--- input_cifar10/convert_to_tf_example.py
+++ input_cifar10/convert_to_tf_example.py
@@ -1,16 +1,18 @@
+"""Convert raw Cifar10 data to tf.Example protos."""
+
 import tensorflow as tf
 
 import os
-import logging
 import sys
 sys.path.append("../utils")
-from mylogging import logger
+
+from tensorflow.python.platform import tf_logging as logging
 
 FLAGS = tf.flags.FLAGS
 
 tf.flags.DEFINE_string("data_dir", "cifar-10-batches-py", "Directory contains data")
 
-tf.flags.DEFINE_string("output_dir", "../data_cifar10", "Directory to store outputs")
+tf.flags.DEFINE_string("output_dir", "../tmp/", "Directory to store outputs")
 
 NUM_FILE_BATCHES = 5
 
@@ -27,7 +29,7 @@ def convert(data, filename):
   num_examples = images.shape[0]
   with tf.python_io.TFRecordWriter(filename) as writer:
     for i in xrange(num_examples):
-      logger.info("Writing batch " + str(i) + "/" + str(num_examples))
+      logging.info("Writing batch " + str(i) + "/" + str(num_examples))
       image = [int(x) for x in images[i, :]]
       label = labels[i]
       example = tf.train.Example()
@@ -37,6 +39,7 @@ def convert(data, filename):
       writer.write(example.SerializeToString())
 
 def main(_):
+  tf.logging.set_verbosity(tf.logging.INFO)
   for i in xrange(NUM_FILE_BATCHES):
     data = unpickle(os.path.join(FLAGS.data_dir, "data_batch_" + str(i + 1)))
     convert(data, os.path.join(FLAGS.output_dir, "data_batch_" + str(i + 1)))
