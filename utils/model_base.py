aafb74cefcf2f9b6f131753c50e197524c47789a
Khanh Nguyen <nguyenxuankhanhm@gmail.com>
2016-09-06 07:46:20 +0700

    Add comments to model base

--- utils/model_base.py
+++ utils/model_base.py
@@ -1,11 +1,28 @@
+"""Define a class to specify a tensorflow model architecture."""
 
 class ModelBase(object):
-
   def __init__(self, is_training):
+    """Initialize a `ModelBase` object.
+      Args:
+        is_training: a bool, whether the model is used for training or
+          evaluation.
+    """
     self.is_training = is_training
 
   def arg_scope(self):
+    """Configurate model's layers.
+
+      Raises:
+        NotImplementedError: this method is required to be implemented by any
+          subclass of `ModelBase`.
+    """
     raise NotImplementedError
 
   def compute(self):
+    """Compute output of the model from a batch of examples.
+
+      Raises:
+        NotImplementedError: this method is required to be implemented by any
+          subclass of `ModelBase`.
+    """
     raise NotImplementedError
